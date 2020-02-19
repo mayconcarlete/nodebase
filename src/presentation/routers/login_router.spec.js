bfc0787bf98249db42b8f2a1cc05f931f85f0f35
Maycon Carlete <mayconcarlete@MacBook-Pro-de-Maycon.local>
2020-02-19 10:41:40 -0300

    ensure LoginRouter return 500 if httpRequest or httpRequest.body is not provided

--- src/presentation/routers/login_router.spec.js
+++ src/presentation/routers/login_router.spec.js
@@ -1,5 +1,15 @@
 class LoginRouter {
     route(httpRequest) {
+        if (!httpRequest) {
+            return {
+                statusCode: 500
+            }
+        }
+        if (!httpRequest.body) {
+            return {
+                statusCode: 500
+            }
+        }
         const { email, password } = httpRequest.body
         if (!email || !password) {
             return {
@@ -31,5 +41,17 @@ describe('Login Router', () => {
         const sut = new LoginRouter()
         httpResponse = sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Should Return 500 if httpRequest is not provided', () => {
+        const sut = new LoginRouter()
+        httpResponse = sut.route()
+        expect(httpResponse.statusCode).toBe(500)
+    })
+    test('Should Return 500 if httpRequest has not body', () => {
+        const sut = new LoginRouter()
+        const httpRequest = {}
+        httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(500)
     })
 })
