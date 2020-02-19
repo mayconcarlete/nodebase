bdefb6d642564ce48c33972f635cd8c9215f245c
Maycon Carlete <mayconcarlete@MacBook-Pro-de-Maycon.local>
2020-02-19 10:28:41 -0300

    feat: ensure LoginRouter return 400 if no email is  provided

--- src/presentation/routers/login_router.spec.js
+++ src/presentation/routers/login_router.spec.js
@@ -1,18 +1,30 @@
 class LoginRouter {
     route(httpRequest) {
-        if (!httpRequest.body.email) {
+        if (!httpRequest.body.email || !httpRequest.body.password) {
             return {
                 statusCode: 400
             }
         }
+
     }
 }
 
 describe('Login Router', () => {
-    test('Should Return 400 if no email is provided', () => {
+    test('Should Return 400 if email is not provided', () => {
         httpRequest = {
             body: {
                 password: 'any_password'
+            }
+        }
+        const sut = new LoginRouter()
+        httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Should Return 400 if password is not provided', () => {
+        httpRequest = {
+            body: {
+                email: 'any_email'
             }
         }
         const sut = new LoginRouter()
