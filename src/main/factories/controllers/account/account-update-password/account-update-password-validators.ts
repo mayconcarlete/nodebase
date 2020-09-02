62a6ed1dd316f55f28b497c6d0639918e9bfdf77
Maycon Carlete <maycon.carlete@hotmail.com>
2020-09-02 15:25:25 -0300

    feat: add lenght validator to password and new password

--- src/main/factories/controllers/account/account-update-password/account-update-password-validators.ts
+++ src/main/factories/controllers/account/account-update-password/account-update-password-validators.ts
@@ -1,4 +1,4 @@
-import { ValidatorComposite, CheckFields, CompareFieldsValidator } from "../../../../../validators";
+import { ValidatorComposite, CheckFields, CompareFieldsValidator, LengthValidator } from "../../../../../validators";
 
 export const makeAccountUpdatePasswordValidator = ():ValidatorComposite =>{
     const validatorsArray = []
@@ -7,6 +7,8 @@ export const makeAccountUpdatePasswordValidator = ():ValidatorComposite =>{
     for(const field of fields){
         validatorsArray.push(new CheckFields(field))
     }
+    validatorsArray.push(new LengthValidator('password', 6, 10))
+    validatorsArray.push(new LengthValidator('newPassword', 6, 10))
     validatorsArray.push(new CompareFieldsValidator('newPassword', 'newPasswordConfirmation'))
     
     return new ValidatorComposite(validatorsArray)
