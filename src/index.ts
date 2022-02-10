10a2935dc0d690cd3daed81b869952e79d0336c1
Maycon Carlete <maycon.carlete@gmail.com>
2022-02-09 19:07:19 -0500

    feat: reduce with functions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import express, {Request, Response, NextFunction} from 'express'
-import {validateEmail, validatePassword} from './middlewares'
+// import express, {Request, Response, NextFunction} from 'express'
+// import {validateEmail, validatePassword} from './middlewares'
 
-const app = express()
+// const app = express()
 
-app.post('/', validateEmail, validatePassword)
+// app.post('/', validateEmail, validatePassword)
 
-const PORT = 4000
-app.listen(PORT, () => {
-  console.log('we are online on port: ', PORT)
-})
+// const PORT = 4000
+// app.listen(PORT, () => {
+//   console.log('we are online on port: ', PORT)
+// })
+const a = (val) => val
+const b = (val) => `b${val}`
+const c = (val) => `c${val}`
+
+type composite = (fn: Function[]) => void
+
+const compositeApp = (...fns) => {
+  return (val) => {
+    return fns.reduce((acc, fn) => fn(acc), val)
+  }
+}
+
+console.log(compositeApp(a,b,c)('z'))
