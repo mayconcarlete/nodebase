c7f7a0cc48a5a1d87b7cacad1294ddf8ccef5db3
Maycon Carlete <maycon.carlete@gmail.com>
2022-02-11 18:23:33 -0500

    feat: add http server

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,11 @@
-// import express, {Request, Response, NextFunction} from 'express'
-// import {validateEmail, validatePassword} from './middlewares'
+import express from 'express'
+import {validateEmail, validatePassword} from './middlewares'
 
-// const app = express()
+const app = express()
 
-// app.post('/', validateEmail, validatePassword)
+app.post('/', validateEmail, validatePassword)
 
-// const PORT = 4000
-// app.listen(PORT, () => {
-//   console.log('we are online on port: ', PORT)
-// })
-
-
-const a = (req, res, next) => {
-  req.param = 1
-  next()
-}
-const b = (req, res, next) => {
-  req.body = 2
-  next()
-}
-
-
-type composite = (fn: Function[]) => void
-
-const compositeApp = (...fns) => {
-  return (req, res, next) => {
-    return fns.reduce((acc, fn) => fn(acc.req, acc.res, acc.next), {req, res, next})
-  }
-}
-
-console.log(compositeApp(a, b)({param:{}, body:{}}, {}, () => console.log('next')))
+const PORT = 4000
+app.listen(PORT, () => {
+  console.log('we are online on port: ', PORT)
+})
